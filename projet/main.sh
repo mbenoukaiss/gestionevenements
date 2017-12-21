dfe75168054ca6513303cdc17b54e13ccf6cc00e
Marwane <marwane.benou-kaiss@etu.u-bordeaux.fr>
2017-12-21 11:19:33 +0100

    Mise en ligne du fichier actuel

--- projet/main.sh
+++ projet/main.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+case "$1" in
+-l)
+	tag="$2"
+	afficherRendezVousAVenir ;;
+-a)
+	afficherAllRendezVous ;;
+-r)
+	tag="$2"
+	supprimerHeure ;;
+-h)
+	usage ;;
+-q)
+	if verifieProcessus = 0
+	then  stopProcessus fi ;;
+*)
+	usage
+	exit 1
+esac
