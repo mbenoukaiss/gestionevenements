487fba6d95716162f5766f016c602a139462d8c8
Marwane <marwane.benou-kaiss@etu.u-bordeaux.fr>
2017-12-21 11:17:03 +0100

    Mise en ligne du fichier actuel

--- projet/processus.sh
+++ projet/processus.sh
@@ -1,1 +1,87 @@
 #!/bin/bash
+
+CONFIGQUITTER=~/.config/quitter/
+FICHIERPID=~/.config/quitter/boucle.pid
+
+# Vérifie que les dossiers et les fichiers nécessaires
+# au fonctionnement du script existent, et les créé si
+# ils n'existent pas.
+function setupConfig {
+	if [ ! -e $CONFIGQUITTER ]
+	then
+		mkdir $CONFIGQUITTER
+	fi
+
+	if [ ! -e $FICHIERPID ]
+	then
+		touch FICHIERPID
+	else
+		if [ ! -f $FICHIERPID ]
+		then
+			rm -r $FICHIERPID
+			touch $FICHIERPID
+		fi
+	fi
+}
+
+# Vérifie qu'il y a un processus valide dans le fichier
+# de processus.
+#
+# return 1 : Le fichier de processus est vide
+# return 2 : Le processus contenu dans le fichier n'est pas valide
+function verifieProcessus {
+	setupConfig
+
+	pid=$(cat FICHIERPID)
+
+	if test -n $pid
+	then
+		if test -n $(ps axu | cut -d' ' -f2 | grep 29992)
+		then
+			return 0
+		else
+			return 2 #LE PROCESSUS N'EST PAS VALIDE
+	else
+		return 1 #FICHIER VIDE
+	fi
+}
+
+# Sauvegarde l'id du processus qui vient d'être créé
+function createProcessus {
+	setupConfig
+
+	pid=$1
+	echo $pid > $FICHIERPID
+}
+
+# Arrête le processus contenu dans le fichier $FICHIERPID
+#
+# return 1 : Le processus contenu dans le fichier n'est
+# 			 pas valide
+function stopProcessus {
+	setupConfig
+	verifieProcessus
+
+	if test $? -eq 0
+	then
+		pid=$(cat $FICHIERPID)
+
+		if test $? -eq 0
+		then
+			kill -9 $pid
+		fi
+
+		echo > FICHIERPID
+	else
+		return 1
+	fi
+}
+
+# Zone de tests
+if test $1 = "c"
+then
+	createProcessus $2
+elif test $1 = "k"
+then
+	stopProcessus
+fi
