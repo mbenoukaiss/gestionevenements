2b7c3c3b20c5b70b2bc0f57ff774e877c257e057
Clément <34739410+Shapeqs@users.noreply.github.com>
2017-12-31 14:48:12 +0100

    Add usage and error function

--- projet/main.sh
+++ projet/main.sh
@@ -1,4 +1,49 @@
 #!/bin/bash
+CHEMINDACCES="~/.config/quitter/"
+FICHIER="horaires.db"
+#HHMM:tag:message
+#tag:chaine (ex tag1+tag2+tag3)
+#ON PEUT L'AMÉLIORER AVEC JOUR ET MOIS EN PLUS
+
+function usage {
+
+echo "usage :   quitter HHMM message... [+tag ...] pour ajouter un rendez vous à l'heure indiquée"
+echo "          quitter -l [+tag ...] pour afficher la liste des rendez-vous à venir"
+echo "          quitter -a pour afficher la liste de tous les rendez-vous"
+echo "          quitter -r [HHMM] pour supprimer les rendez-vous de l'heure correspondante"
+echo "          quitter -r [+tag ...] pour supprimer les rendez-vous du tag correspondant"
+echo "          quitter -h pour afficher le manuel de la commande"
+echo "          quitter -q pour quitter le script"
+
+}
+
+function afficherRendezVousAVenir {
+if [ -n "$tag" ]{
+then
+        date '+%H%M'
+        echo −n "Nous sommes le : "
+        date '+%j %m %Y'
+        echo "et il est : "
+        date '+%H:%M'
+        echo "Voici vos prochains rendez-vous"
+        cut -d: -f1-3 $CHEMINDACCES$FICHIER
+} else {
+}
+}
+
+function afficherAllRendezVous {
+
+less $CHEMINDACCES$FICHIER | cut -d: -f1-3
+
+}
+
+function error {
+
+echo "Vous devez choisir une option parmi : \"-l\", \"-a\", \"-r\" , \"-h\" ou \"-q\"."
+echo "Pour en savoir davantage, faite simplement : \" quitter.sh -h \"."
+
+}
+
 
 case "$1" in
 -l)
@@ -15,6 +60,6 @@ case "$1" in
 	if verifieProcessus = 0
 	then  stopProcessus fi ;;
 *)
-	usage
+	error
 	exit 1
 esac
