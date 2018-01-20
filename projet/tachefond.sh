79a4d53151d27f76ed2ec5b0c2a1384618e82bdf
Marwane <marwane.benou-kaiss@etu.u-bordeaux.fr>
2018-01-20 16:49:51 +0100

    Finalisation de la tâche de fond

--- projet/tachefond.sh
+++ projet/tachefond.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-RENDEZVOUS=/tmp/.config/quitter/
-FICHIERRENDEZVOUS=/tmp/.config/quitter/horaires.db
+RENDEZVOUS=~/.config/quitter/
+FICHIERRENDEZVOUS=~/.config/quitter/horaires.db
 
 while true
 do
     while read ligne
     do
+        decomposeLigne $ligne
+	currenttime=$(date +"%H%M")
 
-    done
+        if test $currenttime = $fheurechaine
+        then
+            chaineVersHeure $fheurechaine
+            echo -e "Il est $fheures:$fminutes et vous avez un rendez-vous : \n$fmessage \nTAGS : $ftags" | xmessage -file - &
+        fi
+    done < $FICHIERRENDEZVOUS
 
     sleep 30
 done
